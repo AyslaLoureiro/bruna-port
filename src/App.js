6415b3c1b2aac5a2e8ba952419670af66830c438
Aysla <ayslaloureiro@gmail.com>
2024-09-06 01:32:12 +0100

    import img

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./bru.jfif";
+import logo1 from "./fisiologo.png";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <>
+          <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo1} className="App-logo" alt="logo" />
+        </>
+        <p>Bru da Fisio!</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.instagram.com/bruna.lopesstefani?igsh=cnpqbnB3Y205d2Jl"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Insta da Bru!
         </a>
       </header>
     </div>
